02ce79a82d8760744e350a2a1da8a59daea814c7
Hyun Woo <hwsa10041@gmail.com>
2023-08-06 13:19:46 +0900

    chore : 권한으로 막혀있던 API Docs 해제

--- src/main/java/dnd/project/global/config/SecurityConfig.java
+++ src/main/java/dnd/project/global/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((request) -> request
                         .requestMatchers(
                                 "/api/v1/version", "/auth","/auth/admin",
-                                "/login/kakao", "/login/google"
+                                "/login/kakao", "/login/google", "/docs/*"
                         )
                         .permitAll()
                 )
