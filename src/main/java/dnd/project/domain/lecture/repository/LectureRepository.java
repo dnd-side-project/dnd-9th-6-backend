5acb76e3271ee7e7d72ed6b3ccb176504c79ad85
Hyun Woo <hwsa10041@gmail.com>
2023-08-08 11:46:51 +0900

    chore : 강의 도메인 수정 및 Repository 생성

---
+++ src/main/java/dnd/project/domain/lecture/repository/LectureRepository.java
@@ -0,0 +1,9 @@
+package dnd.project.domain.lecture.repository;
+
+import dnd.project.domain.lecture.entity.Lecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
+}
