86e17b89a731a4cb1b08799f6928f6af32a0b898
Hyun Woo <hwsa10041@gmail.com>
2023-08-09 16:30:20 +0900

    docs : 후기 수정 API Docs 작성

--- src/test/java/dnd/project/docs/review/ReviewControllerDocsTest.java
+++ src/test/java/dnd/project/docs/review/ReviewControllerDocsTest.java
@@ -145,4 +145,77 @@ public class ReviewControllerDocsTest extends RestDocsSupport {
                         )
                 ));
     }
+
+    @DisplayName("후기 수정 API")
+    @Test
+    void updateReview() throws Exception {
+        // given
+        given(reviewService.updateReview(any(), any()))
+                .willReturn(
+                        ReviewResponse.Create.builder()
+                                .reviewId(1L)
+                                .lectureId(1L)
+                                .userId(1L)
+                                .nickName("클래스코프")
+                                .score(5.0)
+                                .tags("빠른 답변,이해가 잘돼요,보통이에요")
+                                .content("강의가 만족스럽습니다!")
+                                .createdDate("2023-08-09")
+                                .build()
+                );
+
+        ReviewRequest.Update request =
+                new ReviewRequest.Update(1L, 4.0, "빠른 답변,이해가 잘돼요,보통이에요", "강의가 알차고 재밌습니다!");
+
+        // when // then
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.patch("/review")
+                                .header("Authorization", "Bearer AccessToken")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("update-review",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization")
+                                        .description("발급된 JWT AccessToken")
+                        ),
+                        requestFields(
+                                fieldWithPath("reviewId").type(NUMBER)
+                                        .description("강의 ID"),
+                                fieldWithPath("score").type(NUMBER)
+                                        .description("후기 점수 / Double 1~5"),
+                                fieldWithPath("tags").type(STRING)
+                                        .description("후기 태그"),
+                                fieldWithPath("content").type(STRING)
+                                        .description("후기 내용")
+                                        .optional()
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(NUMBER)
+                                        .description("상태 코드"),
+                                fieldWithPath("message").type(STRING)
+                                        .description("상태 메세지"),
+                                fieldWithPath("data.reviewId").type(NUMBER)
+                                        .description("후기 ID"),
+                                fieldWithPath("data.lectureId").type(NUMBER)
+                                        .description("강의 ID"),
+                                fieldWithPath("data.userId").type(NUMBER)
+                                        .description("유저 ID"),
+                                fieldWithPath("data.nickName").type(STRING)
+                                        .description("유저 닉네임"),
+                                fieldWithPath("data.score").type(NUMBER)
+                                        .description("후기 점수 / Double"),
+                                fieldWithPath("data.tags").type(STRING)
+                                        .description("후기 태그"),
+                                fieldWithPath("data.content").type(STRING)
+                                        .description("후기 내용 / 없을시 빈 문자열"),
+                                fieldWithPath("data.createdDate").type(STRING)
+                                        .description("후기 작성 날짜")
+                        )
+                ));
+    }
 }
