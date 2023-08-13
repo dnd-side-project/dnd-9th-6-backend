ce92882e2c7b8d97580baf06959f0562e9c08676
Hyun Woo <hwsa10041@gmail.com>
2023-08-13 17:41:43 +0900

    docs : 후기 키워드 조회 API Docs 작성

--- src/test/java/dnd/project/docs/review/ReviewControllerDocsTest.java
+++ src/test/java/dnd/project/docs/review/ReviewControllerDocsTest.java
@@ -2,6 +2,7 @@ package dnd.project.docs.review;
 
 
 import dnd.project.docs.RestDocsSupport;
+import dnd.project.domain.lecture.response.LectureScopeListReadResponse;
 import dnd.project.domain.review.controller.ReviewController;
 import dnd.project.domain.review.request.ReviewRequest;
 import dnd.project.domain.review.response.ReviewResponse;
@@ -444,6 +445,75 @@ public class ReviewControllerDocsTest extends RestDocsSupport {
                                         .description("강사 이름"),
                                 fieldWithPath("data[].lecture.imageUrl").type(STRING)
                                         .description("강의 이미지 URL")
+                        )
+                ));
+    }
+
+    @DisplayName("후기 키워드 검색 API")
+    @Test
+    void readKeywordReview() throws Exception {
+        // given
+        LectureScopeListReadResponse.DetailReview detailReview1 = new LectureScopeListReadResponse.DetailReview(
+                1L, "The RED : 백엔드 에센셜 : 대용량 서비스를 위한 아키텍처 with Redis by 강대명", "https://fastcampus.co.kr/dev_red_kdm",
+                "김준환", "2023-08-08", 4.5, "강의가 아주 알차고 재밌습니다. 백엔드 개발 화이팅", "뛰어난 강의력,구성이 알차요,도움이 많이 됐어요"
+        );
+
+        LectureScopeListReadResponse.DetailReview detailReview2 = new LectureScopeListReadResponse.DetailReview(
+                2L, "MSA 환경의 효율적인 DevOps를 위한 Istio", "https://fastcampus.co.kr/dev_online_istio",
+                "천현우", "2023-07-23", 4.5, "강의가 아주 알차고 재밌습니다. 백엔드 개발 화이팅", "뛰어난 강의력,이해가 잘돼요,도움이 많이 됐어요"
+        );
+
+        LectureScopeListReadResponse.DetailReview detailReview3 = new LectureScopeListReadResponse.DetailReview(
+                3L, "기본적인 양식 요리부터 심화 단계까지 by Erling Haaland", "https://i.namu.wiki/i/U_ewityI8zmTrUbeUAD8IksVCqy8bRFvGev3Dqpp_10Fu-Tr1zidmr6bcdy9d_UEbuGj_uD3OkimkvUh_8t23ehmymYsoNyWwJG_rkubdG2LbwCUdGz3ug-_NjFhQHjz4aXHvuiRGIeiXJRk66CPdw.webp",
+                "하예은", "2023-08-23", 5.0, "제가 들어본 요리 강의중에 가장 홀란스러운 요리 강의입니다.", "듣기 좋은 목소리,내용이 자세해요,도움이 많이 됐어요"
+        );
+
+        List<LectureScopeListReadResponse.DetailReview> highScoreReviews =
+                List.of(detailReview1, detailReview2, detailReview3);
+
+        ReviewRequest.Keyword request = new ReviewRequest.Keyword("커리큘럼과 똑같아요");
+
+        given(reviewService.readKeywordReview(any(ReviewRequest.Keyword.class)))
+                .willReturn(highScoreReviews);
+
+        // when // then
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/review/keyword")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("read-review-keyword",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("keyword").type(STRING)
+                                        .description("검색 분야")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(NUMBER)
+                                        .description("상태 코드"),
+                                fieldWithPath("message").type(STRING)
+                                        .description("상태 메세지"),
+                                fieldWithPath("data[]").type(ARRAY)
+                                        .description("관심 분야 후기 리스트"),
+                                fieldWithPath("data[].id").type(NUMBER)
+                                        .description("후기 ID"),
+                                fieldWithPath("data[].lectureTitle").type(STRING)
+                                        .description("강의 제목"),
+                                fieldWithPath("data[].imageUrl").type(STRING)
+                                        .description("유저 프로필 이미지 URL"),
+                                fieldWithPath("data[].userName").type(STRING)
+                                        .description("유저 이름"),
+                                fieldWithPath("data[].createdDate").type(STRING)
+                                        .description("후기 작성 날짜"),
+                                fieldWithPath("data[].score").type(NUMBER)
+                                        .description("후기 점수"),
+                                fieldWithPath("data[].content").type(STRING)
+                                        .description("후기 내용"),
+                                fieldWithPath("data[].tags").type(STRING)
+                                        .description("후기 태그")
                         )
                 ));
     }
